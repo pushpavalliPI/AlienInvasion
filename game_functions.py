f0898084d7980b5352489d9a35b2519443f97a31
pushpavalliPI <114721876+pushpavalliPI@users.noreply.github.com>
2023-01-09 18:59:03 +0530

    Bullets

--- game_functions.py
+++ game_functions.py
@@ -2,19 +2,26 @@ import sys
 import pygame
 def check_events(ship):
     """Respont to keyboard presses and mouse events"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
+    def check_keydown_events(event,ship):
+            """Responding to keypresses"""
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+    def check_keyup_events(event,ship):
+        """Responding to keyreleases"""
+        if event.key == pygame.K_RIGHT:
+            ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            ship.moving_left = False
+    def check_events(ship):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                check_keydown_events(event, ship)
+            elif event.type == pygame.KEYUP:
+                check_keyup_events(event, ship)
                 
 def update_screen(ai_settings,screen,ship):
     """Update images on the screen and flip to the new screen."""
